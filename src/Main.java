e2b087b73d4330e9ecfe9829cacfc957e090a50e
546pvp <546pvp@protonmail.com>
2024-08-23 11:54:19 +0200

    Added little "explanation"

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Player dcrffih = new Player("dcrffih");
-        Player saladcool = new Player("saladcool");
-        Player tushikvi = new Player("tushikvi");
+        //create players
+        Player player = new Player("player");
+        Player player2 = new Player("player2");
 
-        saladcool.weapon.shoot(saladcool,dcrffih);
-        tushikvi.weapon.shoot(tushikvi,saladcool);
-        dcrffih.weapon.shoot(dcrffih,saladcool);
-        tushikvi.weapon.shoot(tushikvi,saladcool);
+        /*
+         shoot(Player attacker, Player target);
 
+         - attacker is the one shooting (its only for logging purposes).
+         - target is for logical purposes. It gives him damage, which is then logged into console.
+         */
+
+        player.weapon.shoot(player, player2);
+
+        // send message into the chat!
+        player.sendMessage("Hello World!");
+
+        //these are all interactive methods (for now), there are also getters (check Player.java and Weapon.java);
     }
 }
